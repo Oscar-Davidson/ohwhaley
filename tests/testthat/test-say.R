a626216ae124de26d19854a3dc9e21bd81c6d596
Oscar-Davidson <oscar.davidson@students.mq.edu.au>
2025-09-17 16:47:01 +1000

    Added Tests

---
+++ tests/testthat/test-say.R
@@ -0,0 +1,13 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+# test_that("Description of test",{
+#   expect_(actual, expectation)
+# })
+
+test_that("Output is of correct format",{
+  expect_length(say(), 0) #Returns something with a length of 0
+  expect_null(say()) #Returns null
+  expect_invisible(say()) #Returns invisibly
+  expect_message(say()) #Returns a message
+})
